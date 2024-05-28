db2efcfe74e1cb3b349f065d8cb46036293a27b9
SHANICERUGURU <rugurushanice990@gmail.com>
2024-05-28 11:05:47 +0300

    commit2

--- js/inclass.js
+++ js/inclass.js
@@ -22,6 +22,7 @@
     sampleNumbers ="0123456789"
     sampleSpecial ="#\\|*&%^$£!@"
     sampleChars="abcdefghijklmnopqrstuvwxyz"
+    sampleCharsu="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     function checkpassword(password, regular) {
         for(i =0;i<regular.length;i++){
             if(password.includes(regular[i])){
@@ -32,7 +33,7 @@
     }
     function validatePassword(){
         let password = document.getElementById("input").value
-        if(checkpassword(password, sampleNumbers) && checkpassword(password, sampleSpecial)&&checkpassword(password,sampleChars) &&(password.length>8)){
+        if(checkpassword(password, sampleNumbers) && checkpassword(password, sampleSpecial)&&checkpassword(password,sampleChars) &&(password.length>8)&& checkpassword(password,sampleCharsu)){
             alert("Correct input")
         } else if(!checkpassword(password,sampleChars)){
                 alert("does not contain letters")
@@ -45,7 +46,9 @@
             else if(password.length<8){
                                 alert("add more charachters")
                             }
-            else if ()               
+            else if (!checkpassword(password,sampleCharsu)){
+                alert("does not contain uppercase")
+            }               
             
             else{
             alert("Incorrect")
